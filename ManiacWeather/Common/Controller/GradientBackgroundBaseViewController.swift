3d289ab1215a2f02877e620853d1991d2c75ae38
plizak <>
2020-01-09 18:51:27 +0100

    - Adding missing file (GradientBackgroundBaseViewController)

---
+++ ManiacWeather/Common/Controller/GradientBackgroundBaseViewController.swift
@@ -0,0 +1,26 @@
+
+//
+//  GradientBaseViewController.swift
+//  ManiacWeather
+//
+//  Created by Peter Lizak on 09/01/2020.
+//  Copyright © 2020 Peter Lizak. All rights reserved.
+//
+
+import UIKit
+
+class GradientBackgroundBaseViewController: BaseViewController {
+    
+    var gradientBackgroundStartColor: CGColor {
+        return UIColor.red.cgColor
+    }
+    
+    var gradientBackgroundEndColor: CGColor {
+        return UIColor.blue.cgColor
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        view.applyGradientBackground(colors: [gradientBackgroundStartColor, gradientBackgroundEndColor])
+    }
+}
