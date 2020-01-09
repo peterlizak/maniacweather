bbd8fcf89c747d76cdb93c0e41805f862f4af40b
plizak <>
2020-01-09 17:15:45 +0100

    - Initial commit

---
+++ ManiacWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ManiacWeather
+//
+//  Created by Peter Lizak on 09/01/2020.
+//  Copyright © 2020 Peter Lizak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
